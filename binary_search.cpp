dac11e89ce8a97a33d1240aafb260c00b586e056
Akshat Khare <cs1160315@iitd.ac.in>
2019-04-12 23:00:49 +0530

    binary done, gitignore updated

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,27 +1,71 @@
 //Sample file for students to get their code running
-
-#include<iostream>
-#include "file_manager.h"
-#include "errors.h"
-#include<cstring>
-
-using namespace std;
-
-
+#include "binary_search.h"
+bool binarySearchPage(int searchint, PageHandler ph, FileHandler fh, int startpagenum, int lastpagenum, int * finpage, int * pageoffset){
+	bool found=false;
+	char *data;
+	int num;
+	while(true){
+		if(startpagenum>lastpagenum){
+			found=false;
+			break;
+		}
+		int midpagenum = (startpagenum+lastpagenum)/2;
+		ph = fh.PageAt(midpagenum);
+		int currentpagenum = ph.GetPageNum();
+		data = ph.GetData();
+		vector<int> vec;
+		for(int i=0;i<1024;i++){
+			memcpy(&num, &data[i*4],sizeof(int));
+			if(num==INT_MIN){
+				// cout<< "end of page\n";
+				break;
+			}
+			// cout <<num<<endl;
+			vec.push_back(num);
+		}
+		bool foundvec = binary_search(vec.begin(),vec.end(),searchint);
+		fh.UnpinPage(midpagenum);
+		if(foundvec){
+			*finpage=midpagenum;
+			auto temp = find(vec.begin(),vec.end(),searchint);
+			*pageoffset = distance(vec.begin(), temp);
+			found=true;
+			break;
+		}else{
+			if(searchint<vec[0]){
+				lastpagenum=midpagenum-1;
+			}else if(searchint>vec[vec.size()-1]){
+				startpagenum=midpagenum+1;
+			}else{
+				found=false;
+				break;
+			}
+		}
+	}
+	return found;
+}
 int main(int argc, const char* argv[]) {
 	FileManager fm;
+	FileHandler fh;
+	PageHandler ph;
+	fh = fm.OpenFile(argv[1]);
+	int searchint = atoi(argv[2]);
+	// cout << argv[1]<<endl;
+	// cout << searchint<<endl;
+	ph = fh.FirstPage();
+	int startpagenum= ph.GetPageNum();
+	fh.UnpinPage(startpagenum);
+	ph = fh.LastPage();
+	int lastpagenum= ph.GetPageNum();
+	fh.UnpinPage(lastpagenum);
+	int num;
+	int finpage;
+	int pageoffset;
+	bool found= binarySearchPage(searchint,ph,fh,startpagenum,lastpagenum,&finpage,&pageoffset);	
+	if(found){
+		cout << finpage<<","<<pageoffset<<endl;
+	}else{
+		cout << "-1,-1"<<endl;
+	}
 
-	// Create a brand new file
-	FileHandler fh = fm.CreateFile("temp.txt");
-	cout << "File created " << endl;
-
-	// Create a new page
-	PageHandler ph = fh.NewPage ();
-	char *data = ph.GetData ();
-
-	
-
-	// Close the file and destory it
-	fm.CloseFile (fh);
-	fm.DestroyFile ("temp.txt");
 }
---
+++ binary_search.h
@@ -0,0 +1,10 @@
+#ifndef BINARY_SEARCH_H
+#define BINARY_SEARCH_H
+#include<iostream>
+#include "file_manager.h"
+#include "errors.h"
+#include<cstring>
+#include <bits/stdc++.h> 
+using namespace std;
+bool binarySearchPage(int searchint, PageHandler ph, FileHandler fh, int startpagenum, int lastpagenum, int * finpage, int * pageoffset);
+#endif
