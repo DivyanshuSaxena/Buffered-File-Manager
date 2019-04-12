06b165a6fa723c8d6504f534bf636b68141bd22c
Akshat Khare <cs1160315@iitd.ac.in>
2019-04-13 04:22:02 +0530

    doing merge

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ sampleObjects = buffer_manager.o file_manager.o sample_run.o
 binaryObjects = buffer_manager.o file_manager.o binary_search.o
 insertionObjects = buffer_manager.o file_manager.o insertion.o
 mergeObjects = buffer_manager.o file_manager.o merge_sort.o
+debugObjects = buffer_manager.o file_manager.o debug.o
 
 sample_run : $(sampleObjects)
 	     g++ -std=c++11 -o sample_run $(sampleObjects)
@@ -15,6 +16,9 @@ insertion : $(insertionObjects)
 merge_sort : $(mergeObjects)
 		 g++ -std=c++11 -o merge_sort $(mergeObjects)
 
+debug : $(debugObjects)
+	     g++ -std=c++11 -o debug $(debugObjects)
+
 binary_search.o : binary_search.cpp
 	g++ -std=c++11 -c binary_search.cpp
 
@@ -27,6 +31,9 @@ merge_sort.o : merge_sort.cpp
 sample_run.o : sample_run.cpp
 	g++ -std=c++11 -c sample_run.cpp
 
+debug.o : debug.cpp
+	g++ -std=c++11 -c debug.cpp
+
 buffer_manager.o : buffer_manager.cpp
 	g++ -std=c++11 -c buffer_manager.cpp
 
@@ -35,6 +42,6 @@ file_manager.o : file_manager.cpp
 
 clean :
 	rm -f *.o
-	rm -f sample_run binary_search insertion merge_sort
+	rm -f sample_run binary_search insertion merge_sort debug
 
 
--- merge_sort.cpp
+++ merge_sort.cpp
@@ -4,24 +4,71 @@
 #include "file_manager.h"
 #include "errors.h"
 #include<cstring>
+#include <bits/stdc++.h> 
 
 using namespace std;
 
 
 int main(int argc, const char* argv[]) {
 	FileManager fm;
+	FileHandler fh;
+	PageHandler ph;
+	char*data;
+	int*datai;
+	// int * personalbuffer = (int *)malloc(PAGE_CONTENT_SIZE* sizeof(int));
+	fh = fm.OpenFile(argv[1]);
+	//create runs
+	int runiter=0;
+	int pagestartiter=0;
+	int num;
+	bool pagesnotover=true;
+	int end;
+	while(pagesnotover){
+		vector<int> endindex;
+		vector<int> pageindex;
+		vector<int> sortindex;
+		for(int pageiter=pagestartiter;pageiter<pagestartiter+ BUFFER_SIZE-1;pageiter++){
+			try
+			{
+				ph = fh.PageAt(pageiter);
+			}
+			catch(const std::exception& e)
+			{
+				pagesnotover=false;
+				break;
+			}
+			pageindex.push_back(pageiter);
+			data = ph.GetData();
+			// memcpy(personalbuffer)
+			for(int i=0;i<PAGE_CONTENT_SIZE/4;i++){
+				memcpy(&num,&data[i*4],sizeof(int));
+				if(num==INT_MIN){
+					end=i;
+					break;
+				}
+			}
+			datai = (int *)data;
+			sort(&datai[0],&datai[end]);
+			endindex.push_back(end);
+			sortindex.push_back(0);
+			if(pageiter==pagestartiter+ BUFFER_SIZE-2){
+				pagestartiter=pageiter+1;
+				break;
+			}
+		}
+		//now merge all the pages into a file
+		string s = to_string(runiter);
+		char const *pchar = s.c_str();
+		string runstr("run");
+		char* runptr = &runstr[0];
+		strcat(runptr, pchar);
+		strcat(runptr,".txt");
+		FileHandler fhrun = fm.CreateFile(runptr);
+		PageHandler phrun;
+		while(true){
+			phrun = fhrun.NewPage();
+			
+		}
 
-	// Create a brand new file
-	FileHandler fh = fm.CreateFile(argv[1]);
-	cout << "File created " << endl;
-
-	// Create a new page
-	PageHandler ph = fh.NewPage ();
-	char *data = ph.GetData ();
-
-	
-
-	// Close the file and destory it
-	fm.CloseFile (fh);
-	fm.DestroyFile (argv[1]);
+	}
 }
